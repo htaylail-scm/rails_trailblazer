ec0d27ae051f8eecf479a1ba662dcd9f1698f120
htaylail-scm <scm.htaylail@gmail.com>
2022-06-14 13:41:37 +0630

    Update posts header check

---
+++ config/initializers/constants.rb
@@ -0,0 +1,23 @@
+module Constants
+    TYPES = {
+      'Admin' => '0',
+      'User' => '1'
+    }.freeze
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    POST_CSV_HEADER = %w[title body]
+    POST_CSV_FORMAT_HEADER = %w[title body]
+    VALID_DATE_FORMAT = /\d{4}-\d{2}-\d{2}/
+end
+module Constants
+EMPLOYMENTTYPES = {
+    'Full Time' => 'Full Time',
+    'Part Time' => 'Part Time'
+}.freeze
+end
+module Constants
+DEPARTMENTS = {
+    'IT' => 'IT',
+    'Office Operation' => 'Office Operation',
+    'Network' => 'Network'
+}.freeze
+end
